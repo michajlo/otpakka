f4e8f992fcf05be04d7c27f6c33b275e22ab824c
Michajlo Matijkiw <michajlo.matijkiw@gmail.com>
2012-04-12 23:55:33 -0400

    GenFsmWorker tests

---
+++ src/test/scala/org/michajlo/otpakka/behaviours/workers/GemFsmWorkerTest.scala
@@ -0,0 +1,69 @@
+package org.michajlo.otpakka.behaviours.workers
+
+import org.junit.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.michajlo.otpakka.behaviours.GenFsm
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.Matchers
+
+import akka.actor.ActorSystem
+import akka.testkit.TestActorRef
+
+class GemFsmWorkerTest {
+  import GenFsm.FsmState
+
+  implicit var actorSystem: ActorSystem = _
+  
+  var state: FsmState = _
+  
+  var stateData: List[Any] = _
+  
+  var mockGenFsm: GenFsm = _
+  
+  var testActor: TestActorRef[GenFsmWorker] = _
+  
+  var underTest: GenFsmWorker = _
+  
+  @Before
+  def setUp() = {
+    actorSystem = ActorSystem("test")
+    
+    state = mock(classOf[FsmState])
+    stateData = Nil
+    mockGenFsm = mock(classOf[GenFsm])
+    doReturn(('ok, state, stateData)).when(mockGenFsm).init(Matchers.any(classOf[List[Any]]))
+    
+    
+    testActor = TestActorRef(new GenFsmWorker(mockGenFsm, Nil))
+    underTest = testActor.underlyingActor
+  }
+  
+  @After
+  def tearDown() = {
+    actorSystem.shutdown()
+  }
+  
+  @Test
+  def testInitialStateIsAsReturnedByInit() = {
+    // note, this was done in @Before
+    assertEquals(state, underTest.state)
+    assertEquals(Nil, underTest.stateData)
+  }
+  
+  @Test
+  def testStateTransitionsProperly() = {
+    val nextState = mock(classOf[FsmState])
+    val nextStateData = "a" :: Nil
+    val event = 'some_event
+    
+    doReturn(('next_state, nextState, nextStateData)).when(state).apply(Matchers.any(classOf[(Any, Any)]))
+    testActor ! ('gen_event, event)
+    verify(state).apply(Matchers.eq(event, stateData))
+    assertEquals(nextState, underTest.state)
+    assertEquals(nextStateData, underTest.stateData)
+  }
+}
